a98bd8587f33ac44579d4ce65d6401d1b2b5e6a7
Thomas Loubiou <t.loubiou@systonic.fr>
2018-04-21 22:24:37 +0200

    Refactoring block and tetromino

--- World.gd
+++ World.gd
@@ -20,16 +20,14 @@ func _process(delta):
 	
 func spawn_new_tetromino():
 	next_spawn = spawn_rate
-	var tetromino = random_tetromino()
-	tetromino.position.x = (randi() % grid_width) * 10
-	tetromino.position.y = -100
-	tetromino.player = $Player
+	var tetromino = random_tetromino_at(Vector2(randi() % grid_width, 0))
 	$Tetrominos.add_child(tetromino)
 
-func random_tetromino():
-	var tetrominos = ["L", "J", "I", "S", "Square", "T", "Z"]
-	var tetromino_type = tetrominos[randi() % tetrominos.size()]
-	return load("res://Tetromino/Tetromino" + tetromino_type + ".tscn").instance()
+func random_tetromino_at(grid_position):
+	var tetrominos = preload("res://Tetromino/Tetromino.tscn").instance()
+	tetrominos.init(grid_position, tetrominos.get_random_shape(), $Player)
+	tetrominos.move(60)
+	return tetrominos
 
 func _input(event):
    if event is InputEventMouseButton:
@@ -51,13 +49,14 @@ func update_sight_shooting(target):
 	$ShootingSight.points[1] = endPosition
 	
 	var space_state = get_world_2d().direct_space_state
-	var result = space_state.intersect_ray(playerPosition, endPosition, [$Player.get_node("Area2D")])
-	if result:
-	   pass
+	#var result = space_state.intersect_ray(playerPosition, endPosition, [$Player.get_node("Area2D")])
+	#if result:
+	#   pass
 
 func tick():
-	for tetromino in $Tetrominos.get_children():
-		tetromino.tick()
+	#for tetromino in $Tetrominos.get_children():
+	#	tetromino.tick()
+	pass
 
 func _on_Player_action_done():
 	next_spawn -= 1
