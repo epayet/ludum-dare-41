0a8d4f6faa369b0be8c902e459453714b9b77569
Thomas Loubiou <t.loubiou@systonic.fr>
2018-04-21 22:29:07 +0200

    Add missing files

---
+++ Tetromino/Block.gd
@@ -0,0 +1,44 @@
+extends Area2D
+
+signal moved
+
+var player
+var next_move
+var is_moving = false
+const GRID_SIZE =25
+export (int) var speed = 1
+export (Vector2) var grid_position = Vector2(0, 0)
+
+func _ready():
+	print(grid_position)
+	$Tween.connect("tween_completed", self, "move_done")
+	reset_move()
+
+func init(player, grid_position, speed = 1):
+	self.player = player
+	self.grid_position = grid_position
+	position = grid_position * GRID_SIZE
+	self.speed = speed
+	
+	connect("area_entered", self, "object_entered_area")
+
+func reset_move():
+	next_move = Vector2(0, speed)
+	
+func move(duration):
+	is_moving = true
+	grid_position = grid_position + next_move
+	$Tween.interpolate_property(self, "position", position, grid_position * GRID_SIZE, duration, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	$Tween.start()
+
+
+func move_done(object, key):
+	is_moving = false
+	reset_move()
+	if position.y > get_viewport().size.y + 100:
+		queue_free()
+	emit_signal("moved")
+	
+func object_entered_area(other_area):
+	if other_area == player:
+		print("player dead")
