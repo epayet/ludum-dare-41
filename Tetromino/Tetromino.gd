7138199d31d80c81c2c5ff59045a7e4da60cfd45
Eptwalabha <damiengabrielle@gmail.com>
2018-04-21 16:28:08 +0200

    its ok Mani

--- Tetromino/Tetromino.gd
+++ Tetromino/Tetromino.gd
@@ -1,18 +1,19 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+signal hit
+export (bool) var orientation = false
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
 func tick():
 	position.y += 30
+
+func _on_Area2D_area_entered(area):
+	print(area.get_name())
+	if area.get_name() == "player":
+		print("oups")
+		print("oups")
+	print("collision")
