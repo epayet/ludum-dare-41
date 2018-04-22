e471c2a40a711ff0ce9540b1f374c90d55695802
Thomas Loubiou <t.loubiou@systonic.fr>
2018-04-22 03:08:43 +0200

    Fix player grid

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,39 +7,35 @@ enum DIRECTION {
 	RIGHT
 }
 
+var OFFSET = Vector2(Consts.GRID_CELL_SIZE/2, Consts.GRID_CELL_SIZE/2)
 var can_do_action = false
 var moving = false
 var origin = Vector2()
-var current_position = Vector2()
+var grid_position = Vector2(floor(Consts.GRID_WIDTH / 2), Consts.GRID_HEIGHT - 1)
 export (float) var time_unit = .2
-var SCALE = 30
-var time_left_before_next_move = 0
 
 func _ready():
-	SCALE = get_parent().SCALE
-	current_position = position / SCALE
-	current_position.x = floor(current_position.x)
-	current_position.y = floor(current_position.y)
-	time_left_before_next_move = 0
 	get_node("Tween").connect("tween_completed", self, "on_player_action_completed")
 	moving = false
 
 func _process(delta):
-	if !moving and can_do_action:
-		if (Input.is_action_pressed("ui_left")) && (current_position.x > 0):
-			move_to(LEFT)
-		if Input.is_action_pressed("ui_right") && (current_position.x < Consts.GRID_WIDTH):
-			move_to(RIGHT)
-	position = current_position * SCALE
+	if !moving: 
+		position = grid_position * Consts.GRID_CELL_SIZE + OFFSET
+		if can_do_action:
+			if (Input.is_action_pressed("ui_left")) && (grid_position.x > 0):
+				move_to(LEFT)
+			if Input.is_action_pressed("ui_right") && (grid_position.x < Consts.GRID_WIDTH - 1):
+				move_to(RIGHT)
 
 func move_to(direction):
 	moving = true
-	var next_position = current_position + get_direction(direction)
+	grid_position = grid_position + get_direction(direction)
+	var next_position = grid_position * Consts.GRID_CELL_SIZE + OFFSET
 	var tween = get_node("Tween")
 	tween.interpolate_property(
-				self, "current_position",
-				current_position, next_position,
-				time_unit, Tween.TRANS_LINEAR, 0)
+				self, "position",
+				position, next_position,
+				time_unit, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
 
 func on_player_action_completed(object, property):
