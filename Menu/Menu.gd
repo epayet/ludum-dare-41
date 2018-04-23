bcfdc0492f0bbd11300997739d314e1e42c8b882
Thomas Loubiou <t.loubiou@systonic.fr>
2018-04-23 19:21:53 +0200

    Menu animation

--- Menu/Menu.gd
+++ Menu/Menu.gd
@@ -1,10 +1,28 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	$MainMenu/content/CreditButton.connect("pressed", self, "show_credits")
+	$MainMenu/content/RulesButton.connect("pressed", self, "show_rules")
+	$Credits/BackToMenu.connect("pressed", self, "show_menu")
+	$Rules/BackToMenu.connect("pressed", self, "show_menu")
+	$MainMenu/content/PlayButton.connect("pressed", self , "play")
+	
+	$MainMenu/anim.play("Start")
+	$MainMenu/anim.connect("animation_finished", self, "anim_finished")
+
+func show_credits():
+	$Credits.visible = true
+
+func show_menu():
+	$Credits.visible = false
+	$Rules.visible = false
+	
+func show_rules():
+	$Rules.visible = true
+	
+func play():
+	$MainMenu/anim.play("Play")
+
+func anim_finished(anim_name):
+	if anim_name == "Play":
+		get_tree().change_scene("res://Game.tscn")
